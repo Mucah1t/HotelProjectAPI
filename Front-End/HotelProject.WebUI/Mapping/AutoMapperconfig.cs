86f9befe4c2d64b6e35497910e97d4f627d37cf3
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-07-06 13:39:09 +0300

    The bookingprocess has been completed. in this context, related tables, controllers and views have been added to the project.

--- Front-End/HotelProject.WebUI/Mapping/AutoMapperconfig.cs
+++ Front-End/HotelProject.WebUI/Mapping/AutoMapperconfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.DTOs.AboutDTO;
+using HotelProject.WebUI.DTOs.BookingDTO;
 using HotelProject.WebUI.DTOs.LoginDTO;
 using HotelProject.WebUI.DTOs.RegisterDTO;
 using HotelProject.WebUI.DTOs.ServiceDTO;
@@ -30,6 +31,8 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<ResultStaffDto, Staff>().ReverseMap();
 
             CreateMap<CreateSubscribeDto, Subscribe>().ReverseMap();
+            CreateMap<CreateBookingDto, Booking>().ReverseMap();
+
 
         }
     }
