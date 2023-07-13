ce0866da7989aa55d44e37860eeab4dbcb2ba9b1
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-07-13 16:06:32 +0300

    Jwt bearer mentioned and used in the project, datas in room table made more dynamic by using trigger on sql.

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Test()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Welcome");
+        }
+        [HttpGet("[action]")]
+        public IActionResult TokenCreaateAdmin()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+        [Authorize(Roles ="Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Welcome");
+        }
+    }
+}
---
+++ RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Net.Http.Headers;
+
+namespace RapidApiConsume.Controllers
+{
+    public class SearchLocationIDController : Controller
+    {
+        public async Task<IActionResult> Index(string cityName)
+        {
+            if (!string.IsNullOrEmpty(cityName))
+            {
+                List<BookingApiLocationSearchViewModel> bookingApiLocationSearchViewModel = new List<BookingApiLocationSearchViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityName}&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "de92f704a2mshbb4a9670abd28eep17130ejsn763089a8f18d" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    bookingApiLocationSearchViewModel = JsonConvert.DeserializeObject<List<BookingApiLocationSearchViewModel>>(body);
+                    return View(bookingApiLocationSearchViewModel.Take(1).ToList());
+                }
+            }
+            else
+            {
+                List<BookingApiLocationSearchViewModel> bookingApiLocationSearchViewModel = new List<BookingApiLocationSearchViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Paris&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "de92f704a2mshbb4a9670abd28eep17130ejsn763089a8f18d" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    bookingApiLocationSearchViewModel = JsonConvert.DeserializeObject<List<BookingApiLocationSearchViewModel>>(body);
+                    return View(bookingApiLocationSearchViewModel.Take(1).ToList());
+                }
+            }
+           
+        }
+    }
+}
