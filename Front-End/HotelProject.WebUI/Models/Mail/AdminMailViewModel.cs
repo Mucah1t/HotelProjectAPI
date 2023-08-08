20af5c43a24bcf37dee9e381b9657d55eabed32f
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-08-08 16:59:29 +0300

    Real-time messaging was done via e-mail. APIs were consumed for file upload.

---
+++ Front-End/HotelProject.WebUI/Models/Mail/AdminMailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelProject.WebUI.Models.Mail
+{
+    public class AdminMailViewModel
+    {
+        public string Name { get; set; }
+        public string SenerMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
